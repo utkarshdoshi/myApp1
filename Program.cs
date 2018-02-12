982c59faa31f93995855bfccbe2600415dc844bb
Utkarsh Doshi <utkarsh.doshi@myob.com>
2018-02-13 01:24:19 +1100

    First batch

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace myApp
 {
     class Program
     {
+        static string  echoMe(string str)
+        {return str;}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(""+echoMe("utkarsh."));
         }
     }
 }
