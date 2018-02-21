2723d8d97932f17648c441cf049eb0ad153b39b2
Utkarsh Doshi <utkarsh.doshi@aumel012011m.myob.myobcorp.net>
2018-02-22 08:20:28 +1100

    removed some crap

--- Program.cs
+++ Program.cs
@@ -4,13 +4,10 @@ namespace myapp
 {
     class Program
     {
-        static string  echoMe(string str)
-        {return str;}
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(""+echoMe("Finally, I have dockerized this 'hello world'. "));
+            Console.WriteLine("Finally, I have dockerized this 'hello world'. ");
             Console.WriteLine("Woohoo.... :)");
         }
     }
